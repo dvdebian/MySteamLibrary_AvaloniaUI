b54d91226d97cd7f9bf2db4486f3dadec12e0eef
dvdinux <dvdinux@gmail.com>
2026-01-20 13:33:07 +0100

    removing debug

--- MySteamLibrary/Views/MainView.axaml.cs
+++ MySteamLibrary/Views/MainView.axaml.cs
@@ -15,29 +15,33 @@ public partial class MainView : UserControl
 
     private void OnEffectButtonClicked(object? sender, RoutedEventArgs e)
     {
-        // Find the CarouselView in the visual tree
-        var contentControl = this.FindControl<ContentControl>("ContentControl");
-        if (contentControl == null)
-        {
-            // Try alternative method - find by walking the visual tree
-            var content = this.GetVisualDescendants().OfType<ContentControl>().FirstOrDefault();
-            if (content?.Content is CarouselView carouselView)
-            {
-                carouselView.ToggleEffectOverlay();
-                return;
-            }
-        }
+        //// Find the CarouselView in the visual tree
+        //var contentControl = this.FindControl<ContentControl>("ContentControl");
+        //if (contentControl == null)
+        //{
+        //    // Try alternative method - find by walking the visual tree
+        //    var content = this.GetVisualDescendants().OfType<ContentControl>().FirstOrDefault();
+        //    if (content?.Content is CarouselView carouselView)
+        //    {
+        //        carouselView.ToggleEffectOverlay();
+        //        return;
+        //    }
+        //}
 
-        // If ContentControl found, check its content
-        if (contentControl?.Content is CarouselView carousel)
-        {
-            carousel.ToggleEffectOverlay();
-        }
-        else
-        {
-            // Last resort: find any CarouselView in the visual tree
-            var foundCarousel = this.GetVisualDescendants().OfType<CarouselView>().FirstOrDefault();
-            foundCarousel?.ToggleEffectOverlay();
-        }
+        //// If ContentControl found, check its content
+        //if (contentControl?.Content is CarouselView carousel)
+        //{
+        //    carousel.ToggleEffectOverlay();
+        //}
+        //else
+        //{
+        //    // Last resort: find any CarouselView in the visual tree
+        //    var foundCarousel = this.GetVisualDescendants().OfType<CarouselView>().FirstOrDefault();
+        //    foundCarousel?.ToggleEffectOverlay();
+        //}
+        var foundCarousel = this.GetVisualDescendants().OfType<CarouselView>().FirstOrDefault();
+        foundCarousel?.ToggleEffectOverlay();
+
+
     }
 }
